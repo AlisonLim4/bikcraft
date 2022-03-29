bc9220a9d66a8cce736545723398421522fc43c7
AlisonLima <alison.juniolima@gmail.com>
2022-03-29 11:19:18 -0300

    mostrar / esconder perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -5,8 +6,40 @@ function ativarLink(link) {
   const href = link.href;
 
   if (url.includes(href)) {
-  link.classList.add('ativo');
+    link.classList.add("ativo");
+  }
+}
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+//perguntas frequentes.
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  if (resposta.classList.contains("ativa")) {
+    pergunta.setAttribute("aria-expanded", "true");
+  } else {
+    pergunta.setAttribute("aria-expanded", "false");
   }
 }
 
-links.forEach(ativarLink);
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventoPerguntas);
